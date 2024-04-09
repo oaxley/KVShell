5c56c0e9e52b0508cbd70ad68c6e8438d4440bb3
slegrand <ut5svsxgc@relay.firefox.com>
2024-04-08 22:57:42 -0400

    Add application generic header

---
+++ src/application.h
@@ -0,0 +1,12 @@
+
+
+// ----- guards
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+// ----- includes
+#include "application/args.h"
+#include "application/instance.h"
+#include "application/configuration.h"
+
+#endif // APPLICATION_H
