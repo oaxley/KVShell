cf9c829628e81ec0c12aa03e8fc57ee777146abf
slegrand <ut5svsxgc@relay.firefox.com>
2024-04-20 16:36:58 -0400

    Add support for Help, server and client in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,31 @@
 // main entry point
 
 #include "application.h"
-#include "constants.h"
+#include "kvclient.h"
 #include "kvserver.h"
-#include "network.h"
-
-#include <iostream>
 
 
 int main(int argc, char* argv[])
 {
-    // print program name & version
-    std::cout << Constants::program_name << " - v" << Constants::program_version << "\n";
-
     // setup application instance
     Application::Instance& app = Application::Instance::get();
     app.parse(argc, argv);
     app.loadConfig();
 
+    // check if user wants help
+    if (app.config().is_help) {
+        app.config().printHelp();
+        std::exit(EXIT_SUCCESS);
+    }
+
     // start the TCP Server
     if (app.config().is_server) {
         KVServer kvserver(app.config().srv_address, app.config().srv_port, app.config().database);
         kvserver.start();
+    } else {
+        KVClient kvclient;
+        kvclient.parse(app.cmdline());
     }
-
-
 
     return EXIT_SUCCESS;
 }
